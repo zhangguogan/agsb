ff041c25f32acb5dba02426e9ee2e7cf5926a453
zhangguogan <zhangguogan11@gmail.com>
2025-06-06 13:07:29 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
+USER_NAME = "zgg"         # 用户名
+UUID = "2707593f-1310-4c79-bcd8-fafb2daae1f7"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = ""                   # 域名，留空则自动获取
+CF_TOKEN = "cloudflared.exe service install eyJhIjoiN2VhODQ4ZTk1MzkyNzIxNTE3Y2VjOTMxNzgxZDA2NDkiLCJ0IjoiZTNlMDg1MTQtNzI4Ny00OWYzLTkyMjItOWE2YjZmNzg0YTk2IiwicyI6Ik5tUXlNVE14WXpZdFpqWTNZeTAwT0RFNExXSTVZakl0TVRRNE5tSmpNMkk1TXpaaiJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
